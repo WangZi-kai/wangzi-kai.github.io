dafb11f98cff3d205140a32b8efdc3455675f36e
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2020-10-05 12:24:30 +0800

    Create script7.js

---
+++ js/script7.js
@@ -0,0 +1,29 @@
+function SetHome(obj){
+
+ 	try{ 	obj.style.behavior='url(#default#homepage)';
+
+ 	obj.setHomePage('https://wangzi-kai.github.io/');
+
+ 	}catch(e){
+
+ 	if(window.netscape){
+
+ 	try{
+
+ 	netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect");
+
+ 	}catch(e){
+
+ 	alert("抱歉，此操作被浏览器拒绝！\n\n请在浏览器地址栏输入“about:config”并回车然后将[signed.applets.codebase_principal_support]设置为'true'");
+
+ 	};
+
+ 	}else{
+
+ 	alert("抱歉，您所使用的浏览器无法完成此操作。\n\n您需要手动将'每日科技官网'设置为首页");
+
+ 	};
+
+ 	};
+
+ 	};
