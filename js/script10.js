a1e3c3bc64523c32a41acea550939c541bcb08d1
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2020-10-07 17:01:03 +0800

    Update script10.js

--- js/script10.js
+++ js/script10.js
@@ -1,9 +1,9 @@
 var refurl=document.referrer; 
 
-if(refurl.indexOf("/download/check.html")>-1){
-  alert("对不起，暂时还没有开发mac版本，未来会考虑，感谢支持");
+if(refurl.indexOf("/")>-1){
+  alert("对不起，暂时还没有开发mac、linux版本，未来会考虑，感谢支持");
   window.location.href = "/";
 }
 else{
-window.location.href = "/download/";
+window.location.href = "/";
 }
