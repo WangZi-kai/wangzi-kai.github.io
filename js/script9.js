95023c1e625b03eba4d87e96f56534f8765503cc
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2020-10-07 16:54:19 +0800

    Update script9.js

--- js/script9.js
+++ js/script9.js
@@ -1,107 +1,64 @@
-(function (factory) {
-	if (typeof define === 'function' && define.amd) {
-		// AMD (Register as an anonymous module)
-		define(['jquery'], factory);
-	} else if (typeof exports === 'object') {
-		// Node/CommonJS
-		module.exports = factory(require('jquery'));
-	} else {
-		// Browser globals
-		factory(jQuery);
-	}
-}(function ($) {
+function setCookie(cname, cvalue, exdays) {
+            var d = new Date();
+            d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+            var expires = "expires=" + d.toGMTString();
+            document.cookie = cname + "=" + cvalue + "; " + expires;
+        }
 
-	var pluses = /\+/g;
+        function getCookie(cname) {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i].trim();
+                if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+            }
+            return "";
+        }
 
-	function encode(s) {
-		return config.raw ? s : encodeURIComponent(s);
-	}
+        function check_os() {
+            if (navigator.userAgent.indexOf("Windows") != -1) {
+                return "windows"
+            }
+            if (navigator.userAgent.indexOf("Android") != -1) {
+                return "android"
+            }
+            return "linux"
+        }
 
-	function decode(s) {
-		return config.raw ? s : decodeURIComponent(s);
-	}
 
-	function stringifyCookieValue(value) {
-		return encode(config.json ? JSON.stringify(value) : String(value));
-	}
-
-	function parseCookieValue(s) {
-		if (s.indexOf('"') === 0) {
-			// This is a quoted cookie as according to RFC2068, unescape...
-			s = s.slice(1, -1).replace(/\\"/g, '"').replace(/\\\\/g, '\\');
-		}
-
-		try {
-			// Replace server-side written pluses with spaces.
-			// If we can't decode the cookie, ignore it, it's unusable.
-			// If we can't parse the cookie, ignore it, it's unusable.
-			s = decodeURIComponent(s.replace(pluses, ' '));
-			return config.json ? JSON.parse(s) : s;
-		} catch(e) {}
-	}
-
-	function read(s, converter) {
-		var value = config.raw ? s : parseCookieValue(s);
-		return $.isFunction(converter) ? converter(value) : value;
-	}
-
-	var config = $.cookie = function (key, value, options) {
-
-		// Write
-
-		if (arguments.length > 1 && !$.isFunction(value)) {
-			options = $.extend({}, config.defaults, options);
-
-			if (typeof options.expires === 'number') {
-				var days = options.expires, t = options.expires = new Date();
-				t.setMilliseconds(t.getMilliseconds() + days * 864e+5);
-			}
-
-			return (document.cookie = [
-				encode(key), '=', stringifyCookieValue(value),
-				options.expires ? '; expires=' + options.expires.toUTCString() : '', // use expires attribute, max-age is not supported by IE
-				options.path    ? '; path=' + options.path : '',
-				options.domain  ? '; domain=' + options.domain : '',
-				options.secure  ? '; secure' : ''
-			].join(''));
-		}
-
-		// Read
-
-		var result = key ? undefined : {},
-			// To prevent the for loop in the first place assign an empty array
-			// in case there are no cookies at all. Also prevents odd result when
-			// calling $.cookie().
-			cookies = document.cookie ? document.cookie.split('; ') : [],
-			i = 0,
-			l = cookies.length;
-
-		for (; i < l; i++) {
-			var parts = cookies[i].split('='),
-				name = decode(parts.shift()),
-				cookie = parts.join('=');
-
-			if (key === name) {
-				// If second argument (value) is a function it's a converter...
-				result = read(cookie, value);
-				break;
-			}
-
-			// Prevent storing a cookie that we couldn't decode.
-			if (!key && (cookie = read(cookie)) !== undefined) {
-				result[name] = cookie;
-			}
-		}
-
-		return result;
-	};
-
-	config.defaults = {};
-
-	$.removeCookie = function (key, options) {
-		// Must not alter options, thus extending a fresh object...
-		$.cookie(key, '', $.extend({}, options, { expires: -1 }));
-		return !$.cookie(key);
-	};
-
-}));
+        function DoDownload() {
+            var sl = document.getElementById("sl_os")
+            if (sl.value == "windows") {
+                window.open('https://wwe.lanzous.com/i4ZDUfwxs3g')
+                var blk = document.getElementById('tag_windows')
+                blk.style.backgroundColor = '#fff'
+                blk.style.boxShadow = '2px 2px 2px #000'
+                var bd = document.getElementsByTagName('body')[0]
+                bd.style.backgroundColor = '#aaa'
+            } else if (sl.value == 'linux') {
+                window.open('/download/nosupport.html')
+                var blk = document.getElementById('tag_linux')
+                blk.style.backgroundColor = '#fff'
+                blk.style.boxShadow = '2px 2px 2px #000'
+                var bd = document.getElementsByTagName('body')[0]
+                bd.style.backgroundColor = '#aaa'
+            } else if (sl.value == 'android') {
+                window.open('https://')
+                var blk = document.getElementById('tag_android')
+                blk.style.backgroundColor = '#fff'
+                blk.style.boxShadow = '2px 2px 2px #000'
+                var bd = document.getElementsByTagName('body')[0]
+                bd.style.backgroundColor = '#aaa'
+            } else if (sl.value == 'mac') {
+                window.open('/download/nosupport.html')
+                var blk = document.getElementById('tag_mac')
+                blk.style.backgroundColor = '#fff'
+                blk.style.boxShadow = '2px 2px 2px #000'
+                var bd = document.getElementsByTagName('body')[0]
+                bd.style.backgroundColor = '#aaa'
+            } else if (sl.value == 'ios') {
+                location.href = 'https://'
+                return
+            }
+            location.href = '#tag_' + sl.value
+        }
