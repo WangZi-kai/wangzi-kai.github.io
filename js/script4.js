e81f5d07f75098d99e1a5e870c698e832af065f8
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2020-10-08 11:36:49 +0800

    Update script4.js

--- js/script4.js
+++ js/script4.js
@@ -1,28 +1,12 @@
-var browser = {
-    versions: function () {
-        var u = navigator.userAgent, app = navigator.appVersion;
-        return { 
-        ios: !!u.match(/\(i[^;]+;( U;)? CPU.+Mac OS X/), 
-        android: u.indexOf('Android') > -1 || u.indexOf('Linux') > -1, 
-        iPhone: u.indexOf('iPhone') > -1, 
-        iPad: u.indexOf('iPad') > -1, 
-    };
-}(),
-}
-
-var refurl=document.referrer; 
-  if(refurl.indexOf("/download/")>-1){
-
-if (browser.versions.iPhone || browser.versions.iPad || browser.versions.ios) {
-    window.location.href = "/download/everyday.ipa";
-}
-else if (browser.versions.android) {
-    window.location.href = "/download/everyday.apk";
-}
-else{
-    window.location.href = '/download/check.html'; 
-}
-  }
-else{
-    window.location.href = "/download/";
-}
+var audioEle = document.getElementById("audio");
+var audioControlBtn = jQuery("body").find(".audio-control-btn");
+  audioEle.load();
+  jQuery(".audio-control-btn").click(function() {
+    if (audioEle.paused){ 
+        audioEle.play();   
+        audioControlBtn.addClass("playing")
+    }else {
+        audioEle.pause();  
+        audioControlBtn.removeClass("playing")
+    }
+})
