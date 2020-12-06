b6629100221dfb7ed6d0e21d104af73b0eeb3afd
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2020-12-06 11:50:54 +0800

    Create script12.js

---
+++ js/script12.js
@@ -0,0 +1,1 @@
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1279498743'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1279498743%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
