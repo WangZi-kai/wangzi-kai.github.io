d21efaa1822f44a990f6f5c8e0211cc90dafd80e
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2020-12-31 17:33:27 +0800

    Update script14.js

--- js/script14.js
+++ js/script14.js
@@ -1,5 +1,5 @@
 var password = "ahsb182467953RI";
-var enter = document.getElementsByClassName("button");
+var enter = document.getElementsByClassName("input");
 function login(){
 if(enter == password){
 alert("登录成功");
