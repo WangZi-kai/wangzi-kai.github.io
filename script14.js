fbfbb997176e8a14e428975a7b9f04c150bc4198
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2020-12-31 11:35:01 +0800

    Create script14.js

---
+++ script14.js
@@ -0,0 +1,32 @@
+
+var password = 13787595634;
+var input = document.getElementById("password");
+var td = document.getElementsByTagName("td");
+for(var i = 0;i < td.length;i++){
+	td[i].style.color = "white"
+	td[i].style.backgroundColor = "#EAEAEA";
+	td[i].style.borderRadius = "50px";
+}
+function dodown(obj){
+	obj.style.backgroundColor = "darkgray";
+	input.value += obj.id;
+	
+}
+function doup(obj){
+	obj.style.backgroundColor = "#EAEAEA";
+	if(input.value.length == 6){
+		if(input.value == password){
+			alert("登录成功");
+			location.href="/tj/information.html";
+		}else{
+			alert("密码错误，登录失败")
+			input.value = null;
+		}
+	}
+}
+function doenter(obj){
+	obj.style.backgroundColor = "lightgray";
+}
+function doout(obj){
+	obj.style.backgroundColor = "#EAEAEA";
+}
