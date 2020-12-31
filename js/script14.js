99146d5d70764647b1b997b8ea33fc64bea229e6
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2020-12-31 17:00:46 +0800

    Update script14.js

--- js/script14.js
+++ js/script14.js
@@ -1,31 +1,8 @@
-var password = 958764123;
-var input = document.getElementById("password");
-var td = document.getElementsByTagName("td");
-for(var i = 0;i < td.length;i++){
-	td[i].style.color = "white"
-	td[i].style.backgroundColor = "#EAEAEA";
-	td[i].style.borderRadius = "50px";
-}
-function dodown(obj){
-	obj.style.backgroundColor = "darkgray";
-	input.value += obj.id;
-	
-}
-function doup(obj){
-	obj.style.backgroundColor = "#EAEAEA";
-	if(input.value.length == 9){
-		if(input.value == password){
-			alert("登录成功");
-			location.href="/tj/information.html";
-		}else{
-			alert("密码错误，登录失败")
-			input.value = null;
-		}
-	}
-}
-function doenter(obj){
-	obj.style.backgroundColor = "lightgray";
-}
-function doout(obj){
-	obj.style.backgroundColor = "#EAEAEA";
+var password = ahsb182467953RI;
+var enter = document.getElementById("password");
+function login(){
+if(enter == password){
+alert("登录成功");
+location.href="/tj/information.html";}
+else{alert("密码错误，登录失败")}
 }
