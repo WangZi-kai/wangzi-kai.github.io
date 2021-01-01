386800734fbc1e6846eb98999c51929a6c4887ea
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-01-01 18:46:00 +0800

    Update script5.js

--- js/script5.js
+++ js/script5.js
@@ -1,21 +1,6 @@
 var refurl=document.referrer; 
-  if(refurl.indexOf("/download/jump.html")>-1){
-
-function OSnow(){
-        var agent = navigator.userAgent.toLowerCase();
-        var isMac = /macintosh|mac os x/i.test(navigator.userAgent);
-        if (agent.indexOf("win32") >= 0 || agent.indexOf("wow32") >= 0) {
-            window.location.href = '/download/everyday.zip'; 
-                }
-        if (agent.indexOf("win64") >= 0 || agent.indexOf("wow64") >= 0) {
-            window.location.href = '/download/everyday.zip'; 
-                }
-        if(isMac){
-            window.location.href = '/download/nosupport.html'; 
-                }
-}
-OSnow();
-  }
-else{
-    window.location.href = "/download/";
+if(refurl.indexOf("/download/verify.html")>-1){
+console.log("You are pass the verify! You can see something about this website.")
+}else{
+window.location.href = "/tj/verify.html";
 }
