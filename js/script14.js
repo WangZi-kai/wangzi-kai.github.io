f210169f400e801753ae6bb68802df491b2aa157
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-01-01 09:32:03 +0800

    Update script14.js

--- js/script14.js
+++ js/script14.js
@@ -1,9 +1,10 @@
 function login(){
 var password = "ahsb182467953RI";
 var enter = document.getElementsById("password").value;
-console.log(enter.value);
 if(enter == password){
 alert("登录成功");
-location.href="/tj/information.html";}
-else{alert("密码错误，登录失败")}
+location.href="/tj/information.html";
+}else{
+alert("密码错误，登录失败")
+}
 }
