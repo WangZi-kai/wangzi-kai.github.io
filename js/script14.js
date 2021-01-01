8fcd0ecfa29d53d1e9845c861df430dfa82b144d
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-01-01 09:29:49 +0800

    Update script14.js

--- js/script14.js
+++ js/script14.js
@@ -1,7 +1,7 @@
+function login(){
 var password = "ahsb182467953RI";
 var enter = document.getElementsById("password").value;
 console.log(enter.value);
-function login(){
 if(enter == password){
 alert("登录成功");
 location.href="/tj/information.html";}
