2da4fc6e9b43329d9e424f9a1a24a8bb579492d8
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-01-02 15:46:52 +0800

    Update script5.js

--- js/script5.js
+++ js/script5.js
@@ -2,5 +2,9 @@ var refurl=document.referrer;
 if(refurl.indexOf("/download/verify.html")>-1){
 console.log("You are pass the verify! You can see something about this website.")
 }else{
-window.location.href = "/tj/verify.html";
+swal({title:"您没登录呢！",
+      text:"如果3秒后无反应请<a href='/tj/verify.html'>点击此处<a>！"
+      html: true
+      showConfirmButton: false});
+setTimeout("location.href = '/tj/verify.html'",3000);
 }
