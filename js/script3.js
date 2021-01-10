9cf4f1e33b17dbfa2b91e5c23346bf1188466b0f
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-01-10 13:51:50 +0800

    Update script3.js

--- js/script3.js
+++ js/script3.js
@@ -1,20 +1,14 @@
-    $(window).on("load",function(){
-      var winHeight = $(window).height();
-      function is_weixin() {
-        var ua = window.navigator.userAgent.toLowerCase();
-        if (ua.match(/MicroMessenger/i) == "micromessenger"||ua.match(/QQ/i) == "qq"){
-          return true;
-        }
-        
-        else {
-          return false;
-        }
-      }
-      var isWeixin = is_weixin();
-      if(isWeixin){
-        $(".weixin-tip").css("height",winHeight);
-        $(".weixin-tip").show();
-      }else{
-          window.location.href="/download/jump.html";
-      }
-    })
+let url = navigator.userAgent.toLowerCase();
+//使用toLowerCase将字符串全部转为小写 方便我们判断使用
+if (ua.match(/MicroMessenger/i) == "micromessenger" || ua.match(/QQ/i) == "qq") {
+  //UA判断微信QQ内置浏览器，QQ浏览器会误杀，所以我建议切换电脑UA，这样可以破，wx,qq就切不了UA了
+    if (navigator.userAgent.match(/(iPhone|iPod|iPad|Mac);?/i)) { //判断是否是iOS
+        document.getElementById("WeChatAndroid").style.display='none';
+    }
+    if (navigator.userAgent.match(/android/i)) { //判断是否是Android
+        document.getElementById("WeChatiPhone").style.display='none';
+    }
+}else if(true){
+    document.getElementById("WeChatAndroid").style.display='none';
+    document.getElementById("WeChatiPhone").style.display='none';
+}
