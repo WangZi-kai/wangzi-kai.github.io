832e5210807a0be078b538a1cde30a77c16b9d46
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-01-22 10:42:45 +0800

    Update script21.js

--- js/script21.js
+++ js/script21.js
@@ -4,7 +4,7 @@ function play(){
   swal({
     title:"这张卡片里有没有你想的数字",
     text:"有就点“是”，没有就点“否”",
-    imageUrl: "i/age/1.jpg"
+    imageUrl: "/i/numguess/1.jpg"
     confirmButtonColor: "#DD6B55",
     confirmButtonText: "是",
     cancelButtonText: "否",
@@ -19,4 +19,96 @@ function play(){
       swal("好的👌🏻","继续下一个！","success");
     }
   });
-       
+  swal({
+    title:"这张卡片里有没有你想的数字",
+    text:"有就点“是”，没有就点“否”",
+    imageUrl: "/i/age/2.jpg"
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: "是",
+    cancelButtonText: "否",
+    closeOnConfirm: false,
+    closeOnCancel: false
+  },
+       function(isConfirm){
+    if (isConfirm) {
+      var num = num+2;
+      swal("好的👌🏻","继续下一个！","success");
+    } else {
+      swal("好的👌🏻","继续下一个！","success");
+    }
+  });
+ swal({
+    title:"这张卡片里有没有你想的数字",
+    text:"有就点“是”，没有就点“否”",
+    imageUrl: "/i/numguess/3.jpg"
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: "是",
+    cancelButtonText: "否",
+    closeOnConfirm: false,
+    closeOnCancel: false
+  },
+       function(isConfirm){
+    if (isConfirm) {
+      var num = num+4;
+      swal("好的👌🏻","继续下一个！","success");
+    } else {
+      swal("好的👌🏻","继续下一个！","success");
+    }
+  }); 
+  swal({
+    title:"这张卡片里有没有你想的数字",
+    text:"有就点“是”，没有就点“否”",
+    imageUrl: "/i/numguess/4.jpg"
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: "是",
+    cancelButtonText: "否",
+    closeOnConfirm: false,
+    closeOnCancel: false
+  },
+       function(isConfirm){
+    if (isConfirm) {
+      var num = num+8;
+      swal("好的👌🏻","继续下一个！","success");
+    } else {
+      swal("好的👌🏻","继续下一个！","success");
+    }
+  });
+  swal({
+    title:"这张卡片里有没有你想的数字",
+    text:"有就点“是”，没有就点“否”",
+    imageUrl: "/i/numguess/5.jpg"
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: "是",
+    cancelButtonText: "否",
+    closeOnConfirm: false,
+    closeOnCancel: false
+  },
+       function(isConfirm){
+    if (isConfirm) {
+      var num = num+16;
+      swal("好的👌🏻","继续下一个！","success");
+    } else {
+      swal("好的👌🏻","继续下一个！","success");
+    }
+  });
+  swal({
+    title:"这张卡片里有没有你想的数字",
+    text:"有就点“是”，没有就点“否”",
+    imageUrl: "/i/numguess/6.jpg"
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: "是",
+    cancelButtonText: "否",
+    closeOnConfirm: false,
+    closeOnCancel: false
+  },
+       function(isConfirm){
+    if (isConfirm) {
+      var num = num+32;
+      swal("好的👌🏻","继续下一个！","success");
+    } else {
+      swal("好的👌🏻","继续下一个！","success");
+    }
+  });
+  swal("我知道了","你想的数字是"+num,"success");
+  swal()
+  
