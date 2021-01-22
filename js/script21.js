3bad85a3309202b081d2d1e6bfb9bbb073b449c6
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-01-22 10:26:59 +0800

    Update script21.js

--- js/script21.js
+++ js/script21.js
@@ -1,1 +1,22 @@
+var num = 0;
 function play(){
+  swal('请在1~60随便想一个数字');
+  swal({
+    title:"这张卡片里有没有你想的数字",
+    text:"有就点“是”，没有就点“否”",
+    imageUrl: "i/age/1.jpg"
+    confirmButtonColor: "#DD6B55",
+    confirmButtonText: "是",
+    cancelButtonText: "否",
+    closeOnConfirm: false,
+    closeOnCancel: false
+  },
+       function(isConfirm){
+    if (isConfirm) {
+    var num = num+1;
+    swal('好的');
+  } else {
+    swal("OK！");
+  }
+});
+       
