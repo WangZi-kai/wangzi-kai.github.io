0e53ac01c0f096b22322ab5f4f791d8cc3fc895d
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-02-04 15:44:49 +0800

    Update and rename script11.js to baidustatistics.js

--- js/baidustatistics.js
+++ js/baidustatistics.js
@@ -1,14 +1,8 @@
 var _hmt = _hmt || [];
-
 (function() {
-
   var hm = document.createElement("script");
-
   hm.src = "https://hm.baidu.com/hm.js?ce79d61c8ff5066c0fd31693354aaa67";
-
   var s = 
 document.getElementsByTagName("script")[0];
- 
   s.parentNode.insertBefore(hm, s);
-
 })();
