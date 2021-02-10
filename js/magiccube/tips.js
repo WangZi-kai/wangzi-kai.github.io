8663d02efe5740b300c04efc77995cc0caa63d89
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-02-10 20:04:39 +0800

    Update tips.js

--- js/magiccube/tips.js
+++ js/magiccube/tips.js
@@ -1,28 +1,5 @@
-var ua = navigator.userAgent;
-var ipad = ua.match(/(iPad).*OS\s([\d_]+)/),
-    isIphone = !ipad && ua.match(/(iPhone\sOS)\s([\d_]+)/),
-    isAndroid = ua.match(/(Android)\s+([\d.]+)/),
-    isMobile = isIphone || isAndroid;
-    if(isMobile) {
-        swal({
-  title: "提示",
-  text: "本游戏对手机<span style="color:red">兼容性不好<span>，去玩玩其他游戏吧",
-  type: "warning",
-  showCancelButton: true,
-  confirmButtonColor: "#DD6B55",
-  confirmButtonText: "好的",
-  cancelButtonText: "容我看看",
-  closeOnConfirm: false,
-  closeOnCancel: false,
-  html:ture
-},
-function(isConfirm){
-  if (isConfirm) {
-    location.href="/game/";
-  } else {
-    alert("慢慢看，就打扰你这一下。")
-  }
-});
+if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
+    alert("本游戏对手机兼容性不好，去玩玩其他游戏吧！")
     }else{
         consule.log("建议使用Chrome内核的浏览器访问")
-    ｝
+    }
