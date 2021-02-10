7a1b840b943a9ebe2e415232f9542723dc47ea0b
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-02-10 20:09:52 +0800

    Update tips.js

--- js/magiccube/tips.js
+++ js/magiccube/tips.js
@@ -1,5 +1,10 @@
 if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-    alert("本游戏对手机兼容性不好，去玩玩其他游戏吧！")
+    swal({
+        title: "提示"
+        text: "本游戏对手机兼容性不好，去玩玩其他游戏吧！"
+        timer: 5000,
+        showConfirmButton: false
+    });
     }else{
         consule.log("建议使用Chrome内核的浏览器访问")
     }
