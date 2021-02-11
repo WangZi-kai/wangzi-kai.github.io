9dd8f10301fe335825077b916eca91596052bf3c
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-02-11 12:19:39 +0800

    Update somegametips.js

--- js/somegametips.js
+++ js/somegametips.js
@@ -1,5 +1,5 @@
 if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-    alert("本游戏对手机兼容性不好，去玩玩其他游戏吧！")
+    swal("提示","本游戏对手机兼容性不好，去玩玩其他游戏吧！","warning")
     }else{
         consule.log("建议使用Chrome内核的浏览器访问")
     }
