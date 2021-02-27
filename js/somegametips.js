04f465285bc8115fd3c2aea3c3527274207ef8ed
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-02-27 10:42:56 +0800

    Update somegametips.js

--- js/somegametips.js
+++ js/somegametips.js
@@ -1,7 +1,7 @@
 if(/Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent)) {
-    document.write('<p style="color:orange;font-size:40px"><b>本游戏对手机兼容性不好，去玩玩其他游戏吧！</b></p>');
+    document.write('<p style="color:orange;font-size:40px"><b>对手机兼容性不好，去试试其他的吧！</b></p>');
 }else if(window.navigator.userAgent.indexOf("Chrome") > -1){
-    console.log("浏览器很棒，可以正常兼容这款游戏！");
+    console.log("浏览器很棒，可以正常兼容！");
 }else{
     document.write('<p style="color:orange;font-size:40px"><b>建议使用Chrome内核的浏览器访问！</b></p>');
 }
