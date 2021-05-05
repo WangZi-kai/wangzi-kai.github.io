8e00b91b05fee66516f6eef68a9e67a86c2e6bce
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-05-05 16:13:19 +0800

    Update wechatqq.js

--- js/wechatqq.js
+++ js/wechatqq.js
@@ -1,9 +1,18 @@
     $(window).on("load",function(){
       var winHeight = $(window).height();
-        if (window.navigator.userAgent.toLowerCase().match(/MicroMessenger\/[0-9]/i)||window.navigator.userAgent.toLowerCase().match(/QQ\/[0-9]/i)){
-        $(".weixin-tip").css("height",winHeight);
-        $(".weixin-tip").show();
+      function is_weixin() {
+        var ua = window.navigator.userAgent.toLowerCase();
+        if (ua.match(/MicroMessenger\/[0-9]/i)||ua.match(/QQ\/[0-9]/i)){
+          return true;
         } else {
-          consule.log("非微信，QQ");
+          return false;
         }
       }
+      var isWeixin = is_weixin();
+      if(isWeixin){
+        $(".weixin-tip").css("height",winHeight);
+        $(".weixin-tip").show();
+      }else{
+          consule.log("非微信，QQ");
+      }
+    })
