33c0810149840a8e0948672c2fc7080b54d222ec
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-05-09 13:42:30 +0800

    Update downloader.js

--- js/downloader.js
+++ js/downloader.js
@@ -5,8 +5,22 @@ $(document).ready(function(){
         if (!check.test(link)) {
             alert("网址无效，请重新输入");
         } else {
+            /*以下是以前的思路，FireFox会受到同源限制（大部分浏览器都是使用的FireFox，您可以使用浏览器信息查看工具来看代号，为Mozilla的都是），只有Chrome可以正常下载
             $("#dler").attr("href",link);
             document.getElementById("dler").click();
+            以下是比较好的思路，不会受到限制
+            */
+            var dload = new XMLHttpRequest();
+    dload.open("GET", link, true);
+    dload.responseType = 'blob';
+    dload.onload=function(e) {
+        var url = window.URL.createObjectURL(x.response)
+        var a = document.createElement('a');
+        a.href = link
+        a.download = "hi科技-网站源码下载";
+        a.click()
+    }
+    x.send();
         }
   });
 });
