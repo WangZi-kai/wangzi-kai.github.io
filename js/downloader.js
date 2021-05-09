3444e8c14bab0d1cd0246a16c14f1b25106c3d1c
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-05-09 14:27:26 +0800

    Update downloader.js

--- js/downloader.js
+++ js/downloader.js
@@ -8,19 +8,9 @@ $(document).ready(function(){
             /*以下是以前的思路，还有一个ID为dler的a标签，FireFox会受到同源限制（大部分浏览器都是使用的FireFox，您可以使用浏览器信息查看工具来看代号，为Mozilla的都是），只有Chrome可以正常下载
             $("#dler").attr("href",link);
             document.getElementById("dler").click();
-            以下是比较好的思路，不会受到限制
+            以下是比较好的思路，使用了download.js，不会受到限制
             */
-            var dload = new XMLHttpRequest();
-            dload.open("GET", link, true);
-            dload.responseType = 'blob';
-            dload.onload=function(e) {
-                var url = window.URL.createObjectURL(x.response)
-                var a = document.createElement('a');
-                a.href = link
-                a.download = "hi科技-网站源码下载";
-                a.click()
-            }
-            dload.send();
+            download(document.body.outerHTML,link,"text/html");
         }
   });
 });
