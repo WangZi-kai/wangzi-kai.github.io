19f0166241f5e0a37c4d9b9f1ffc1cf7a0330389
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-05-23 15:10:33 +0800

    Create norobot.js

--- js/norobot.js
+++ js/norobot.js
@@ -2,8 +2,8 @@ function pass(){
   $("#menu").show();
   $.cookie('pass', 'true', { expires: 3 });
 }
-var cookie = document.cookie;
-if (cookie.indexOf("pass=true") != -1) {
+var check = $.cookie("pass")
+if (check == "true") {
   $("#menu").show();
 } else {
   $("#menu").hide();
