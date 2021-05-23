196d1ffa9d873eb9cd230d8923cdb26c01d7f4d3
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-05-23 14:54:56 +0800

    Update norobot.js

--- js/norobot.js
+++ js/norobot.js
@@ -2,7 +2,8 @@ function pass(){
   $("#menu").show();
   $.cookie('pass', 'true', { expires: 3 });
 }
-if (document.cookie.indexOf("pass=true") != -1) {
+var cookie= document.cookie;
+if (cookie.indexOf("pass=true") != -1) {
   $("#menu").show();
 } else {
   $("#menu").hide();
