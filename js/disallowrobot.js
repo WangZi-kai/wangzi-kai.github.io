62534422763231c6c32222f26e58732336a4685d
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-06-06 17:38:18 +0800

    Create disallowrobot.js

---
+++ js/disallowrobot.js
@@ -0,0 +1,12 @@
+    $(function () {
+        var slider = new SliderUnlock("#verify",{
+            labelTip: "验证显示选项",
+            successLabelTip: "验证成功",
+        }, function(){
+            $("#menu").show()
+        }, function(){
+            $("#menu").hide()
+        }
+        slider.init();
+        });
+    })
