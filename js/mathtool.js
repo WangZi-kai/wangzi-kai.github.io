df089b424185278ce169cdfc3049799778105318
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-07-20 21:09:49 +0800

    Update mathtool.js

--- js/mathtool.js
+++ js/mathtool.js
@@ -2,4 +2,4 @@ var check = $.url().param('choose');
 var header = "/tool/math/";
 var footer = ".html";
 var result = header+check+footer;
-$("toolshow").attr("src",result);
+$("#toolshow").attr("src",result);
