1486a3e594d146f2807ffdbae5b75acb04873bb1
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-07-20 22:13:51 +0800

    Update mathtool.js

--- js/mathtool.js
+++ js/mathtool.js
@@ -1,8 +1,6 @@
 window.onload=function(){
 var check = $.url().param('choose');
-if(check == underfined){
-$("#toolshow").attr("src","/tool/math/error.html");
-}else{var header = "/tool/math/";
+var header = "/tool/math/";
 var footer = ".html";
 var result = header+check+footer;
 $("#toolshow").attr("src",result);
