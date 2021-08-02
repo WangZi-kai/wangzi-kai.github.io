fc4eefd12269a6f7b56552a5061a8bfd24a91024
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-08-02 20:30:49 +0800

    Update downloader.js

--- js/downloader.js
+++ js/downloader.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#dl").click(function(){
         link = document.getElementById("link").value;
-        check = /^([hH][tT]{2}[pP]:\/\/|[hH][tT]{2}[pP][sS]:\/\/)(([A-Za-z0-9-_]+)\.)+([A-Za-z0-9-_\/])+$/;
+        check = /^([hH][tT]{2}[pP]:\/\/|[hH][tT]{2}[pP][sS]:\/\/)(([A-Za-z0-9-_]+)\.)+([A-Za-z0-9-_\/])?$/;
         if (!check.test(link)) {
             alert("网址无效，请重新输入");
         } else {
