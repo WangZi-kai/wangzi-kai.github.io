764f0ceaeb2270dcf9445d9bfadade9ef1bf901b
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-08-03 11:10:24 +0800

    Update downloader.js

--- js/downloader.js
+++ js/downloader.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $("#dl").click(function(){
         link = document.getElementById("link").value;
-        check = /^http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ./?%&=]*)?$/;
+        check = /^http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ,./?%&=]*)?$/;
         if (!check.test(link)) {
             alert("网址无效，请重新输入");
         } else {
