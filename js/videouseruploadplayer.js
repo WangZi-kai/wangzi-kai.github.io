f1fb871269d109877aa15b06d7d5dd7d91035f0b
WangZi-kai <68527203+WangZi-kai@users.noreply.github.com>
2021-08-03 11:24:06 +0800

    Update videouseruploadplayer.js

--- js/videouseruploadplayer.js
+++ js/videouseruploadplayer.js
@@ -1,35 +1,28 @@
-    // HTML5实现表单内的上传文件框，上传前预览视频，刷新预览video，使用HTML5 的File API,
-    // 建立一个可存取到该file的url，一个空的video标签，ID为video0,把选择的文件显示在video标签中，实现视频预览功能。
-    // 需要选择支持HTML API的浏览器。
-    $("#video").change(function () {
-        $("#video0").show(); 
-        var objUrl = getObjectURL(this.files[0]);
-        console.log("objUrl = " + objUrl);
-        if (objUrl) {
-            $("#video0").attr("src", objUrl);
-        }
+$("#video").change(function () {
+    $("#video0").show(); 
+    var objUrl = getObjectURL(this.files[0]);
+    if (objUrl) {
+        $("#video0").attr("src", objUrl);
+    }
     });
-    //建立一个可存取到该file的url
-    function getObjectURL(file) {
-        var url = null;
-        if (window.createObjectURL != undefined) { // basic
-            url = window.createObjectURL(file);
-        } else if (window.URL != undefined) { // mozilla(firefox)
-            url = window.URL.createObjectURL(file);
-        } else if (window.webkitURL != undefined) { // webkit or chrome
-            url = window.webkitURL.createObjectURL(file);
-        }
-        return url;
+function getObjectURL(file) {
+    var url = null;
+    if (window.createObjectURL != undefined) { // basic
+        url = window.createObjectURL(file);
+    } else if (window.URL != undefined) { // mozilla(firefox)
+        url = window.URL.createObjectURL(file);
+    } else if (window.webkitURL != undefined) { // webkit or chrome
+        url = window.webkitURL.createObjectURL(file);
     }
-    //粘贴链接播放部分
-    function loadlink(){
-        var link = $("#link").val();
-        //使用正则判断是否是链接
-        check = /^http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ,./?%&=]*)?$/;
-        if (!check.test(link)) {
-            alert("网址无效，请重新输入");
-        } else {
-            $("#video0").show();
-            $("#video0").attr("src",link);
-        }
+    return url;
+}
+function loadlink(){
+    var link = $("#link").val();
+    check = /^http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ,./?%&=]*)?$/;
+    if (!check.test(link)) {
+        alert("网址无效，请重新输入");
+    } else {
+        $("#video0").show();
+        $("#video0").attr("src",link);
     }
+}
