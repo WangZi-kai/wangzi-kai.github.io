c3e972106418a193229ba31e3166dad502f73182
汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>
2022-04-29 08:24:31 +0000

    update js/cnzz.js.

--- js/cnzz.js
+++ js/cnzz.js
@@ -1,1 +1,10 @@
-document.write(unescape("%3Cspan id='cnzz_stat_icon_1279498743'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1279498743%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
+//document.write(unescape("%3Cspan id='cnzz_stat_icon_1279498743'%3E%3C/span%3E%3Cscript src='https://s4.cnzz.com/z_stat.php%3Fid%3D1279498743%26show%3Dpic1' type='text/javascript'%3E%3C/script%3E"));
+var _hmt = _hmt || [];
+(function() {
+      var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?ce79d61c8ff5066c0fd31693354aaa67";
+          var s = 
+          document.getElementsByTagName("script")[0];
+            s.parentNode.insertBefore(hm, s);
+})();
+})
