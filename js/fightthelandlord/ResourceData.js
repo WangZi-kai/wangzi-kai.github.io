cc39356b50c2010e27a7da75a3f16d7607f118f6
汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>
2022-07-28 09:10:07 +0000

    add file via upload

---
+++ js/fightthelandlord/ResourceData.js
@@ -0,0 +1,74 @@
+
+
+var ResourceData= {
+    Images:{
+        //path：文件相对路径，picNum:当前图片中宽和高包含的小图片数，cellSize:小图片的宽和高，data：存放加载的图片数据。
+        //goddess_new:{path:"images/goddess_new.png", picNum:{WNum:4,HNum:4},cellSize:{width:32,height:48}, data:null}
+        1:{path:"/i/fightthelandlord/1.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:18}
+        ,2:{path:"/i/fightthelandlord/2.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:17}
+        ,3:{path:"/i/fightthelandlord/3.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:3}
+        ,4:{path:"/i/fightthelandlord/4.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:4}
+        ,5:{path:"/i/fightthelandlord/5.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:5}
+        ,6:{path:"/i/fightthelandlord/6.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:6}
+        ,7:{path:"/i/fightthelandlord/7.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:7}
+        ,8:{path:"/i/fightthelandlord/8.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:8}
+        ,9:{path:"/i/fightthelandlord/9.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:9}
+        ,10:{path:"/i/fightthelandlord/10.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:10}
+        ,11:{path:"/i/fightthelandlord/11.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:11}
+        ,12:{path:"/i/fightthelandlord/12.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:12}
+        ,13:{path:"/i/fightthelandlord/13.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:13}
+        ,14:{path:"/i/fightthelandlord/14.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:14}
+        ,15:{path:"/i/fightthelandlord/15.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:4,num:16}
+        ,16:{path:"/i/fightthelandlord/16.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:3}
+        ,17:{path:"/i/fightthelandlord/17.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:4}
+        ,18:{path:"/i/fightthelandlord/18.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:5}
+        ,19:{path:"/i/fightthelandlord/19.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:6}
+        ,20:{path:"/i/fightthelandlord/20.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:7}
+        ,21:{path:"/i/fightthelandlord/21.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:8}
+        ,22:{path:"/i/fightthelandlord/22.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:9}
+        ,23:{path:"/i/fightthelandlord/23.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:10}
+        ,24:{path:"/i/fightthelandlord/24.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:11}
+        ,25:{path:"/i/fightthelandlord/25.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:12}
+        ,26:{path:"/i/fightthelandlord/26.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:13}
+        ,27:{path:"/i/fightthelandlord/27.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:14}
+        ,28:{path:"/i/fightthelandlord/28.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:3,num:16}
+        ,29:{path:"/i/fightthelandlord/29.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:3}
+        ,30:{path:"/i/fightthelandlord/30.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:4}
+        ,31:{path:"/i/fightthelandlord/31.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:5}
+        ,32:{path:"/i/fightthelandlord/32.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:6}
+        ,33:{path:"/i/fightthelandlord/33.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:7}
+        ,34:{path:"/i/fightthelandlord/34.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:8}
+        ,35:{path:"/i/fightthelandlord/35.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:9}
+        ,36:{path:"/i/fightthelandlord/36.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:10}
+        ,37:{path:"/i/fightthelandlord/37.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:11}
+        ,38:{path:"/i/fightthelandlord/38.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:12}
+        ,39:{path:"/i/fightthelandlord/39.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:13}
+        ,40:{path:"/i/fightthelandlord/40.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:14}
+        ,41:{path:"/i/fightthelandlord/41.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:2,num:16}
+        ,42:{path:"/i/fightthelandlord/42.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:3}
+        ,43:{path:"/i/fightthelandlord/43.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:4}
+        ,44:{path:"/i/fightthelandlord/44.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:5}
+        ,45:{path:"/i/fightthelandlord/45.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:6}
+        ,46:{path:"/i/fightthelandlord/46.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:7}
+        ,47:{path:"/i/fightthelandlord/47.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:8}
+        ,48:{path:"/i/fightthelandlord/48.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:9}
+        ,49:{path:"/i/fightthelandlord/49.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:10}
+        ,50:{path:"/i/fightthelandlord/50.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:11}
+        ,51:{path:"/i/fightthelandlord/51.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:12}
+        ,52:{path:"/i/fightthelandlord/52.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:13}
+        ,53:{path:"/i/fightthelandlord/53.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:14}
+        ,54:{path:"/i/fightthelandlord/54.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null,se:1,num:16}
+        ,BeiMian:{path:"/i/fightthelandlord/BeiMian.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:105,height:150}, data:null}
+        ,bg1:{path:"/i/fightthelandlord/bg1.png", picNum:{WNum:1,HNum:1},cellSize:{width:640,height:320}, data:null}
+        ,btn:{path:"/i/fightthelandlord/btn.jpg", picNum:{WNum:1,HNum:1},cellSize:{width:469,height:204}, data:null}
+    },
+    Sound:{
+        //soundName:声音文件名称，path：文件夹相对路径，data：存放加载的声音数据。由于各种浏览器对声音格式的支持不一致，声音文件格式有MP3和OGG两种
+        //sound01:{soundName:"sound01",path:"sound/", data:null,num:17}
+    },
+    Animation:{
+         //ZS1_move:{imageName:"goddess_new",beginPoint:{WNum:0,HNum:0},allPlayNum:4,times:2}
+    }
+
+
+};
