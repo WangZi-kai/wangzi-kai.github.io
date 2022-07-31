d110605fff02eef703648c15db4c96364085e367
汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>
2022-07-31 08:17:03 +0000

    删除文件 js/script24.js

--- js/script24.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  verify = $.cookie('verify');
-  if(verify == 'ok'){
-    consule.log('Welcome!');
-  }else{
-    window.location.href('/game/verify.html');
-  }
-});
