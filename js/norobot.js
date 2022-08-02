fb7be880cc97cf07c76a6c3fd42f423cbdeae48b
汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>
2022-08-02 06:08:21 +0000

    update js/norobot.js.

--- js/norobot.js
+++ js/norobot.js
@@ -1,6 +1,7 @@
 $(function () {
     var slider = new SliderUnlock(".slideunlock-slider", {}, function () {
         $("#menu").show();
+        $(".slideunlock-wrapper").hide()
     }, function(){});
     slider.init();
 });
