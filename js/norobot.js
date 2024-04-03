58cc0d78c278e2d81b669e5389d554a11a81408b
汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>
2024-04-03 12:44:23 +0000

    update js/norobot.js.
    Signed-off-by: 汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>

--- js/norobot.js
+++ js/norobot.js
@@ -1,7 +1,13 @@
 $(function () {
     var slider = new SliderUnlock(".slideunlock-slider", {}, function () {
-        $("#menu").show();
+        $("#recaptcha").show();
         $(".slideunlock-wrapper").hide(1000);
     }, function(){});
     slider.init();
 });
+function pass(){
+    $("#menu").show();
+}
+function hide(){
+    $("#menu").hide();
+}
