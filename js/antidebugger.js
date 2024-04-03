105d883aec075dd8b6890d56bbadb6c6fbc37bc7
汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>
2024-04-03 12:05:40 +0000

    add js/antidebugger.js.
    Signed-off-by: 汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>

---
+++ js/antidebugger.js
@@ -0,0 +1,20 @@
+window._windon_handler = setInterval(
+	function(){
+		var before = new Date();
+		debugger;
+		var after = new Date();
+		if (after.getTime() > before.getTime() + 100) {
+			if (after.getTime() > before.getTime() + 100) {
+				window.location.href="about:blank";
+				clearInterval(_windon_handler);
+				}else{
+					Anti_numtots++;
+					if(Anti_numtots>=1){
+						window.location.href="about:blank";
+						clearInterval(_windon_handler);
+					}
+				}
+		}else{
+			Anti_numtots = 0;
+		}
+	}, 1000)
