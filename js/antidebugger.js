663d3d4aa387cb15c16241eab4e2b0770f338061
汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>
2024-04-03 12:18:55 +0000

    update js/antidebugger.js.
    Signed-off-by: 汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>

--- js/antidebugger.js
+++ js/antidebugger.js
@@ -5,11 +5,13 @@ window._windon_handler = setInterval(
 		var after = new Date();
 		if (after.getTime() > before.getTime() + 100) {
 			if (after.getTime() > before.getTime() + 100) {
+                window.location.reload();
 				window.location.href="about:blank";
 				clearInterval(_windon_handler);
 				}else{
 					Anti_numtots++;
 					if(Anti_numtots>=1){
+                        window.location.reload();
 						window.location.href="about:blank";
 						clearInterval(_windon_handler);
 					}
