bcc73a8a0568f5f40a60f3f9d8b46f8213300f52
汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>
2026-06-28 15:13:07 +0000

    update js/htmlview.js.
    Signed-off-by: 汪子凯本人 <7856930+WzkBr@user.noreply.gitee.com>

--- js/htmlview.js
+++ js/htmlview.js
@@ -52,7 +52,7 @@ function getFormattedTime() {
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
     
-    return `${year}-${month}-${day}-${hours}:${minutes}:${seconds}`;
+    return `${year}-${month}-${day}_${hours}-${minutes}-${seconds}`;
 }
 
 toggleDesktop.addEventListener('click', function() {
